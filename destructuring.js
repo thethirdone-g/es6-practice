e6e7ee0123bbf3e84222cc8761a759323c3cdd3f
George McCadden III <thethirdone.g@gmail.com>
2019-05-22 12:27:14 -0700

    Provided a code example of destructuring

---
+++ destructuring.js
@@ -0,0 +1,8 @@
+// This will be a file for practicing the JS destructuring technique, starting with arrays :
+
+const numbers = [1, 2, 3, 4];
+
+// In order to destructure the "numbers" array, the syntax looks like this :
+
+[number1, number2] = numbers; 
+console.log(number1, number2) // This will log the first two index values in the "numbers" array. 
