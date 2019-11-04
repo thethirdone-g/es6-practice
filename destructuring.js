9f2caf66f20d29239b522936364936d91308a908
George McCadden III <thethirdone.g@gmail.com>
2019-11-03 22:23:55 -0800

    destructuring with objects

--- destructuring.js
+++ destructuring.js
@@ -5,4 +5,14 @@ const numbers = [1, 2, 3, 4];
 // In order to destructure the "numbers" array, the syntax looks like this :
 
 [number1, number2] = numbers; 
-console.log(number1, number2) // This will log the first two index values in the "numbers" array. 
+console.log(number1, number2) // This will log the first two index values in the "numbers" array.
+
+// Using destructuring assignment to extract values from objects :
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
